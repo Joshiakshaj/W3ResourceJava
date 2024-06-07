8bdd47693fc324b506e749f653d7a5d1a0c18f1c
Joshiakshaj <129145776+Joshiakshaj@users.noreply.github.com>
2024-06-07 16:40:21 +0530

    Add files via upload
    Write a Java program to compute a specified formula.
    Specified Formula :
    4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))
    Expected Output
    2.9760461760461765

---
+++ BasicQues/exten.java
@@ -0,0 +1,6 @@
+public class exten {
+    public static void main(String[] args) {
+        double num = 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+        System.out.println(num);
+    }
+}
