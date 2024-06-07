fc2639804db7ddf0c8db37f212df32fab397ea12
Joshiakshaj <129145776+Joshiakshaj@users.noreply.github.com>
2024-06-07 14:07:16 +0530

    Add files via upload
    Write a Java program to divide two numbers and print them on the screen.
    Test Data :
    50/3
    Expected Output :
    16

---
+++ BasicQues/division.java
@@ -0,0 +1,12 @@
+//Write a Java program to divide two numbers and print them on the screen.
+//Test Data :
+//50/3
+//Expected Output :
+//16
+
+public class division {
+    public static void main(String[] args) {
+        int div = 50/3;
+        System.out.println(div);
+    }
+}
