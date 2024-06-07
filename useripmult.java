72449647b35c78eadb96ac47d52112f3ed5cf2c2
Joshiakshaj <129145776+Joshiakshaj@users.noreply.github.com>
2024-06-07 14:27:25 +0530

    Exercise 5
    Write a Java program that takes two numbers as input and displays the product of two numbers.
    Test Data:
    Input first number: 25
    Input second number: 5
    Expected Output :
    25 x 5 = 125

---
+++ useripmult.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class useripmult {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Input first number");
+    int num1= sc.nextInt();
+    System.out.println("Input second number");
+    int num2 = sc.nextInt();
+    sc.close();
+    System.out.println(num1*num2);
+}
+}
