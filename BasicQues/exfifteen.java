d300c1455f08331febc74849886678338d77787b
Joshiakshaj <129145776+Joshiakshaj@users.noreply.github.com>
2024-06-08 13:41:23 +0530

    Add files via upload
    Write a Java program to swap two variables.

---
+++ BasicQues/exfifteen.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class exfifteen {
+    public static void main(String[] args) {
+        int a, b, temp;
+
+        a= 11;
+        b= 12;
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swapping : a, b = " + a + ", " + b);
+
+    }
+}
